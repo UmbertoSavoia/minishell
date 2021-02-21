8c5214685516bbdbffef8a26381c61287de85cd3
Umberto Savoia <usavoia@student.42roma.it>
2021-02-21 16:54:18 +0100

    gitignore

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -2,10 +2,6 @@
 
 void	print_prompt(void)
 {
-	/* printf(BGBLK" %s@minishell"NC, (get_value_env("USER"))->value);
-	printf(BGBLUE BLACK""NC);
-	printf(BGBLUE BLACK"%s"NC, (get_value_env("PWD"))->value);
-	printf(BLUE" "NC); */
 	ft_putstr_fd(BGBLK" ", 1);
 	ft_putstr_fd((get_value_env("USER"))->value, 1);
 	ft_putstr_fd("@minishell"NC, 1);
