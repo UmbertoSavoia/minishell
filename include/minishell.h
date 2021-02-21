723bc079f7157ef9c214b75e3ca8ede979896df2
Umberto Savoia <usavoia@student.42roma.it>
2021-02-21 11:34:54 +0100

    add include

---
+++ include/minishell.h
@@ -0,0 +1,17 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/errno.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+
+# include "../libft/libft.h"
+# include "../get_next_line/get_next_line_bonus.h"
+
+#endif
