4013a2e9651fee2587eac2fcd23322c4448448e6
Umberto Savoia <usavoia@student.42roma.it>
2021-02-27 15:12:49 +0100

    error

--- srcs/relative_path.c
+++ srcs/relative_path.c
@@ -9,6 +9,8 @@ void	relative_path(int i)
 	DIR				*folder;
 	struct dirent	*entry;
 	char			**args;
+	int				wstatus;
+	int				status_code;
 
 	pid = 0;
 	tmp = ft_strrchr((char*)g_shell.table_list[i]->content, '/');
@@ -27,8 +29,8 @@ void	relative_path(int i)
 	closedir(folder);
 	if (pid)
 	{
+		wait(&wstatus);
 		pid = fork();
-		wait(NULL);
 		if (pid == 0)
 		{
 			args = ft_split(tmp, ' ');
@@ -36,6 +38,12 @@ void	relative_path(int i)
 			ft_free_arr(args);
 			free(path);
 			built_exit();
+		}
+		else
+		{
+			status_code = WEXITSTATUS(wstatus);
+			if (status_code != 0)
+				printf("\n[%d]\n", status_code);
 		}
 	}
 	free(path);
