82ffbb4f56d9f544c6b0c4b0343d164402a63be0
cromalde <cromalde@student.42roma.it>
2021-03-07 11:49:04 +0100

    fixed storia modificabile e leggibille

--- srcs/print_history.c
+++ srcs/print_history.c
@@ -13,41 +13,42 @@ void	navigate_history(int dir)
 {
 	static int		len;
 	static int		size;
-	int				len_cpy;
 
 	if (!size)
 		size = g_shell.hist_size;
-	printf("[%d]\n", g_shell.hist_size);
 	if (dir == UP && size >= 1)
 	{
-		printf("{%d}\n", size);
-		//clean_prompt(len);
+		write(1, "\x1b[1K", 4);
+		write(1, "\r", 1);
+		print_prompt();
 		len = ft_strlen((char*)g_shell.history->content);
+		ft_memcpy(g_shell.final_string, g_shell.history->content, len);
+		g_shell.len_string = len;
+		g_shell.curs += len;
 		write(1, ((char*)g_shell.history->content), len);
 		(g_shell.history->next) ? g_shell.history = g_shell.history->next : 0;
 		(size != 1) ? size-- : 0;
 		return ;
 	}
-	else if (dir == DW && size < g_shell.hist_size)
+	else if (dir == DW && size < g_shell.hist_size && len)
 	{
-		printf("(%d)\n", size);
 		size++;
 		(g_shell.history->prev) ? g_shell.history = g_shell.history->prev : 0;
-		//clean_promp(len);
+		(g_shell.history->prev) ? g_shell.history = g_shell.history->prev : 0;
+		write(1, "\x1b[1K", 4);
+		write(1, "\r", 1);
+		print_prompt();
 		len = ft_strlen((char*)g_shell.history->content);
+		ft_memcpy(g_shell.final_string, g_shell.history->content, len);
+		g_shell.len_string = len;
+		g_shell.curs += len;
 		write(1, ((char*)g_shell.history->content), len);
 		return ;
 	}
-	else if (size > g_shell.hist_size)
+	else if (size >= g_shell.hist_size)
 	{
-		len_cpy = len;
-		while (len--)
-			write(1, "\b", 1);
-		len = len_cpy;
-		while (len_cpy--)
-			write(1, " ", 1);
-		len_cpy = len;
-		while (len_cpy--)
-			write(1, "\b", 1);
+		write(1, "\x1b[1K", 4);
+		write(1, "\r", 1);
+		print_prompt();
 	}
 }
